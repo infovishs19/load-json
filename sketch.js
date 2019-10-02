951a053b9adb2f03684df9d6d35935d0835647c0
chrischne <chrischne@gmail.com>
2019-10-02 17:40:58 +0200

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,19 @@
+/// <reference path="./libraries/p5.global-mode.d.ts" />
+
+
+function setup () {
+	createCanvas(4 * 800, 600);
+
+	frameRate(30);
+
+}
+
+function draw () {
+
+	background(200);
+
+
+}
+
+
+
